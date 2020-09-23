74d1699b379fa0836dc01dab0c0d216396e07f6d
hprudhomme <prudhommehadrien@gmail.com>
2020-09-24 00:22:39 +0200

    convert base ok

--- C07/ex042/ft_convert_base.c
+++ C07/ex042/ft_convert_base.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int		check_base(char *base);
 int		ft_is_in_base(char *base, char c);
@@ -62,19 +63,23 @@ int		ft_find_res_dec(char c, char *base, int dec)
 	return (dec);
 }
 
-char	*ft_res_final(char *base_to, int dec, char *res_final)
-{
-	int j;
-
-	j = 0;
-	while (dec)
-	{
-		res_final[j] = base_to[dec % ft_str_len(base_to)];
-		dec = dec / ft_str_len(base_to);
-		j++;
-	}
-	return (res_final);
-}
+char	*ft_res_final(char *base_to, int dec, char *res_final, int n)
+{   
+	int res_len;
+	res_len = !(n % 2) ? ft_smllc(base_to, dec) + 1 : ft_smllc(base_to, dec) + 2;
+    res_final[res_len] = '\0';
+    res_len = res_len - 1;
+    while (res_len > n % 2)
+    {   
+        res_final[res_len] = base_to[dec % ft_str_len(base_to)];
+        dec = dec / ft_str_len(base_to);
+        res_len--;
+    }
+	res_final[res_len] = base_to[dec % ft_str_len(base_to)];
+	if (n % 2)
+		res_final[0] = '-';
+    return res_final;
+} 
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
@@ -98,8 +103,16 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 		if (!(res_final = malloc(sizeof(char) *
 				(ft_smllc(base_to, dec) + (n % 2) + 1))))
 			return (NULL);
-		res_final = ft_res_final(base_to, dec, res_final);
-		return (ft_rev_tab(res_final, ft_smllc(base_to, dec) + 1, n));
+		return(res_final = ft_res_final(base_to, dec, res_final, n));
 	}
 	return (NULL);
+}
+
+int main()
+{
+	char n[] = "-15620";
+	char base[] = "01234567";
+	char base2[] = "0123456789";
+
+	printf("%s\n", ft_convert_base(n, base, base2));
 }
